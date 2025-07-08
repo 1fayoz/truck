f1ae9e8a11bbca4f44c2ff9e93eaf3b0408d2852
1fayoz <turaqulovfayoz4@gmail.com>
2025-07-08 11:27:59 +0500

    pagination

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -263,6 +263,6 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 500
-    page_query_param = 'page_size'
+    page_query_param = 'page'
 
 
