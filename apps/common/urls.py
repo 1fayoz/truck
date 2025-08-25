d92708fa7df03af4579b8aa80530852f73fc3e27
kimdir <turaqulovfayoz4@gmail.com>
2025-08-25 16:59:23 +0500

    d

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('user', views.UserLogin.as_view()),
     path('service', views.ServiceView.as_view()),
 
-    path('club/members/', views.DocsView.as_view())
+    path('docs', views.DocsView.as_view())
 
 ]
